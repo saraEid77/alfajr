587746aad341f46ce49fea5d61dbec844ae7a0f5
Sara Eid <sara.eid.77777@gmail.com>
2021-07-17 16:52:31 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,427 @@
+jQuery(function ($) {
+    'use strict';
+	
+        // Header Sticky
+		$(window).on('scroll',function() {
+            if ($(this).scrollTop() > 120){  
+                $('.navbar-area').addClass("is-sticky");
+            }
+            else{
+                $('.navbar-area').removeClass("is-sticky");
+            }
+		});
+
+		// Mean Menu
+		jQuery('.mean-menu').meanmenu({
+			meanScreenWidth: "1199"
+        });
+		
+		// Others Option For Responsive JS
+		$(".others-option-for-responsive .dot-menu").on("click", function(){
+			$(".others-option-for-responsive .container .container").toggleClass("active");
+		});
+
+        // Video Slides
+		$('.video-slides').owlCarousel({
+			loop: true,
+			nav: true,
+			dots: false,
+			rtl: true,
+			autoplayHoverPause: true,
+			autoplay: true,
+            margin: 30,
+            navText: [
+				"<i class='bx bx-chevron-right'></i>",
+                "<i class='bx bx-chevron-left'></i>"
+              
+            ],
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                1200: {
+                    items: 2,
+				}
+            }
+        });
+
+  
+        // photo Slides
+		$('.photoGallary').owlCarousel({
+			loop: true,
+			nav: true,
+			dots: false,
+			rtl: true,
+			autoplayHoverPause: true,
+			autoplay: true,
+            margin: 30,
+            navText: [
+				"<i class='bx bx-chevron-right'></i>",
+                "<i class='bx bx-chevron-left'></i>"
+              
+            ],
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                1200: {
+                    items: 2,
+				}
+            }
+        });
+		  
+
+
+	
+
+
+		//   $('[data-fancybox="images"]').fancybox({
+		// 	buttons : [ 
+		// 	  'slideShow',
+		// 	  'share',
+		// 	  'zoom',
+		// 	  'fullScreen',
+		// 	  'download',
+		// 	  'close',
+			
+		// 	],
+		// 	thumbs : {
+		// 	  autoStart : true,
+	
+		// 	}
+		//   });
+		$('[data-fancybox="images"]').fancybox({
+			baseClass: 'myFancyBox',
+			thumbs: {
+			  autoStart: true,
+			  axis: 'x'
+			}
+		  })
+		  
+
+        // Popup Video
+		$('.popup-youtube').magnificPopup({
+			disableOn: 320,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: false
+		});
+        
+        // Business News Slides
+		$('.business-news-slides').owlCarousel({
+			loop: true,
+			nav: true,
+			rtl: true,
+			dots: false,
+			autoplayHoverPause: true,
+			autoplay: true,
+            margin: 30,
+            navText: [
+				"<i class='bx bx-chevron-right'></i>",
+                "<i class='bx bx-chevron-left'></i>"
+               
+            ],
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 1,
+                },
+                768: {
+                    items: 3,
+                },
+                1200: {
+                    items: 3,
+				}
+            }
+        });
+
+        // Health News Slides
+		$('.health-news-slides').owlCarousel({
+			loop: true,
+			nav: true,
+			rtl: true,
+			dots: false,
+			autoplayHoverPause: true,
+			autoplay: true,
+            margin: 30,
+            navText: [
+                "<i class='bx bx-chevron-left'></i>",
+                "<i class='bx bx-chevron-right'></i>"
+            ],
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                1200: {
+                    items: 2,
+				}
+            }
+        });
+
+        
+		// side options 
+		 $('.o-icon').click (function(event){
+		event.preventDefault();
+		if( $ (this).hasClass('inOut')  ){
+			$('.side-options').stop().animate({right:'0px'},500 );
+		} else{
+			$('.side-options').stop().animate({right:'-200px'},500 );
+		} 
+		$(this).toggleClass('inOut');
+		return false;
+
+    	}  );
+
+		$('.o-icon2').click (function(event){
+			event.preventDefault();
+			if( $ (this).hasClass('inOut')  ){
+				$('.side-options2').stop().animate({right:'0px'},500 );
+			} else{
+				$('.side-options2').stop().animate({right:'-300px'},500 );
+			} 
+			$(this).toggleClass('inOut');
+			return false;
+	
+			}  );
+           //  calender
+			$('.calendar-container').calendar({	
+		    });
+				
+
+       // pray times 
+		// $('.prayer-times-container').prayerTimes({
+		// 	arabic:true
+
+		// });
+
+
+		// Go to Top
+		$(function(){
+			// Scroll Event
+			$(window).on('scroll', function(){
+				var scrolled = $(window).scrollTop();
+				if (scrolled > 600) $('.go-top').addClass('active');
+				if (scrolled < 600) $('.go-top').removeClass('active');
+			});  
+			// Click Event
+			$('.go-top').on('click', function() {
+				$("html, body").animate({ scrollTop: "0" },  500);
+			});
+		});
+
+		// Sports Slides
+		$('.sports-slider').owlCarousel({
+			loop: true,
+			nav: true,
+			rtl: true,
+			dots: false,
+			autoplayHoverPause: true,
+			autoplay: true,
+            items: 1,
+            margin: 30,
+            navText: [
+                "<i class='bx bx-chevron-left'></i>",
+                "<i class='bx bx-chevron-right'></i>"
+			],
+			
+			responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 2,
+				},
+				768: {
+                    items: 1,
+                },
+                1200: {
+                    items: 1,
+				}
+            }
+		});
+		
+		// Tech Slides
+		$('.tech-slider').owlCarousel({
+			loop: true,
+			nav: true,
+			dots: false,
+			rtl: true,
+			autoplayHoverPause: true,
+			autoplay: true,
+            items: 1,
+            margin: 30,
+            navText: [
+                "<i class='bx bx-chevron-left'></i>",
+                "<i class='bx bx-chevron-right'></i>"
+			],
+			
+			responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 2,
+				},
+				768: {
+                    items: 1,
+                },
+                1200: {
+                    items: 1,
+				}
+            }
+        });
+
+		// Breaking News Slides
+		$('.breaking-news-slides').owlCarousel({
+			loop: true,
+			nav: false,
+			rtl: true,
+			dots: false,
+			autoplayHoverPause: true,
+			autoplay: true,
+			animateOut:"slideOutDown",
+			animateIn:"flipInX",
+            items: 1,
+            margin: 30,
+            navText: [
+                "<i class='bx bx-chevron-left'></i>",
+                "<i class='bx bx-chevron-right'></i>"
+            ],
+		});
+
+
+
+		// Main News Slides
+		$('.main-news-slides').owlCarousel({
+			loop: true,
+			nav: true,
+			rtl: true,
+			dots: false,
+			autoplayHoverPause: true,
+			autoplay: true,
+            margin: 30,
+            navText: [
+                "<i class='bx bx-chevron-left'></i>",
+                "<i class='bx bx-chevron-right'></i>"
+            ],
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                576: {
+                    items: 1,
+                },
+                768: {
+                    items: 2,
+                },
+                1200: {
+                    items: 3,
+				}
+            }
+		});
+
+
+		// most slides
+		$('.newsticker1').newsTicker({
+			row_height: 112,
+			speed: 800,
+			prevButton: $('#prev-button1'),
+			nextButton: $('#next-button1')
+		
+		});
+		$('.newsticker2').newsTicker({
+			row_height: 112,
+			speed: 800,
+			prevButton: $('#prev-button2'),
+			nextButton: $('#next-button2')
+		
+		});
+		$('.newsticker3').newsTicker({
+			// row_height: 150,
+			// max_rows: 2,
+			// speed: 500,
+			// direction: 'up',
+			// duration: 2000,
+			// autostart: 1,
+			// pauseOnHover: 0,
+			row_height: 112,
+			speed: 800,
+			prevButton: $('#prev-button3'),
+			nextButton: $('#next-button3')
+		
+		});
+
+		// FAQ Accordion
+        $(function() {
+            $('.accordion').find('.accordion-title').on('click', function(){
+                // Adds Active Class
+                $(this).toggleClass('active');
+                // Expand or Collapse This Panel
+                $(this).next().slideToggle('fast');
+                // Hide The Other Panels
+                $('.accordion-content').not($(this).next()).slideUp('fast');
+                // Removes Active Class From Other Titles
+                $('.accordion-title').not($(this)).removeClass('active');		
+            });
+		});
+
+		// Nice Select JS
+		$('select').niceSelect();
+
+		// Count Time 
+		function makeTimer() {
+			var endTime = new Date("September 20, 3000 17:00:00 PDT");			
+			var endTime = (Date.parse(endTime)) / 1000;
+			var now = new Date();
+			var now = (Date.parse(now) / 1000);
+			var timeLeft = endTime - now;
+			var days = Math.floor(timeLeft / 86400); 
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+			$("#days").html(days + "<span>Days</span>");
+			$("#hours").html(hours + "<span>Hours</span>");
+			$("#minutes").html(minutes + "<span>Minutes</span>");
+			$("#seconds").html(seconds + "<span>Seconds</span>");
+		}
+		setInterval(function() { makeTimer(); }, 0);
+		
+        // Preloader
+        $(window).on('load', function() {
+            $('.preloader').fadeOut();
+            $('.preloader-area').addClass('preloader-deactivate');
+        });
+
+       // responsive tabs
+
+         //open and close tab menu
+		 $('#tab_selector').on('change', function (e) {
+			$('.nav-tabs button').eq($(this).val()).tab('show');
+		});
+
+}(jQuery));
